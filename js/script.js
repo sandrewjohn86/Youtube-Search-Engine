dbebf93d2cce04d6d11fadc7d5102296e4ac99a1
Stephen John <sandrewjohn86@gmail.com>
2017-01-09 20:01:55 -0400

    Add YouTube API

--- js/script.js
+++ js/script.js
@@ -24,4 +24,42 @@ $(function(){
 			}, 400, function(){});
 		}
 	});
+
+	//To prevent the form from submitting
+	$('#search-form').submit(function(e){
+		e.preventDefault();
+	});
 })
+
+function search(){
+	//clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	// Get Form Input
+	q = $('#query').val();
+
+	//Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search", {
+			part: 'snippet, id',
+			q: q,
+			type: 'video',
+			key:'AIzaSyCAGzHosxLpZTNy4LIudAUi9AAMieS0EEI'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+
+				//Log Data
+				console.log(data);
+
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+
+					//Display Results
+					$('#results').append(output);
+				});
+			}
+	);
+}
