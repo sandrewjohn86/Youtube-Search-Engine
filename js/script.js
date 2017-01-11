7597c0b5d77ec3fa860fad34ee5e421d84716ed8
Stephen John <sandrewjohn86@gmail.com>
2017-01-10 23:39:17 -0400

    Add additional styling and light box

--- js/script.js
+++ js/script.js
@@ -69,6 +69,92 @@ function search(){
 	);
 }
 
+// Next Page Function
+function nextPage(){
+	var token = $('#next-button').data('token')
+	var q = $('#next-button').data('query')
+	
+	//clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	// Get Form Input
+	q = $('#query').val();
+
+	//Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search", {
+			part: 'snippet, id',
+			q: q,
+			pageToken:token,
+			type: 'video',
+			key:'AIzaSyCAGzHosxLpZTNy4LIudAUi9AAMieS0EEI'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+
+				//Log Data
+				console.log(data);
+
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+
+					//Display Results
+					$('#results').append(output);
+				});
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);
+}
+
+// Next Page Function
+function prevPage(){
+	var token = $('#prev-button').data('token')
+	var q = $('#prev-button').data('query')
+	
+	//clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	// Get Form Input
+	q = $('#query').val();
+
+	//Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search", {
+			part: 'snippet, id',
+			q: q,
+			pageToken:token,
+			type: 'video',
+			key:'AIzaSyCAGzHosxLpZTNy4LIudAUi9AAMieS0EEI'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+
+				//Log Data
+				console.log(data);
+
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+
+					//Display Results
+					$('#results').append(output);
+				});
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);
+}
+
 
 // Build Output
 function getOutput(item){
@@ -85,7 +171,7 @@ function getOutput(item){
 	'<img src="'+thumb+'">' + 
 	'</div>' +
 	'<div class="list-right">' +
-	'<h3>'+title+'</h3>' +
+	'<h3><a class="fancybox fancybox.iframe" href="https://www.youtube.com/embed/'+videoId+'">'+title+'</a></h3>' +
 	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoData+'</small>' +
 	'</div>' +
 	'</li>' +
